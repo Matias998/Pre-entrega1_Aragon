daf155f3da0901d1c4775ae7e7ee5726035795e3
matiasAragon <m4tthe0.ar@gmail.com>
2022-09-22 01:51:00 -0300

    app creada

---
+++ src/components/ItemListContainer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ItemListContainer = () => {
+   return (
+      <>
+         <section className="container my-4">
+            <div className="row justify-content-center">
+              
+            </div>
+         </section>
+      </>
+   );
+}
+
+export default ItemListContainer;
